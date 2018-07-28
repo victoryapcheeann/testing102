db506d2f5ce0b2c2d534f637c501473f5f63347a
Victor Yap <victoryapcheeann@gmail.com>
2018-07-28 16:42:39 +0800

    Added test for textarea and form

--- src/Components/__tests__/App.test.js
+++ src/Components/__tests__/App.test.js
@@ -1,30 +1,26 @@
 import React from 'react';
-import App from 'components/App';
-import TopicBox from 'components/TopicBox';
-import TopicList from 'components/TopicList';
-/*
-Change to absolute imports, 
-so that I can move to the test file around without breaking import statement in the future
-*/
-import { shallow } from 'enzyme';
-//npm install --save enzyme enzyme-adapter-react-16
 
-let wrapped;
-/*
-Why let not const? 
-We are going to reassign the value of wrapped several times throughout these files
-*/
+import App from 'Components/App'; //Change to absolute imports 
+import TopicsBox from 'Components/TopicsBox'; //So that I can move to the test file around without breaking import statement in the future
+import TopicsList from 'Components/TopicsList';
+
+import { shallow } from 'enzyme'; //npm install --save enzyme enzyme-adapter-react-16
+
+let wrapped; //We are going to reassign the value of wrapped several times throughout these files
 
 beforeEach(() => {
   wrapped = shallow(<App />);
 });
 
-it('shows topic box', ()=>{
-  expect(wrapped.find(TopicBox).length).toEqual(1);
+afterEach(() => {
+  wrapped.unmount(); //Cleaning up to prevent performance issues later on
 });
-//Make sure TopicBox component is inside App component
 
-it('shows topic list', ()=>{
-  expect(wrapped.find(TopicList).length).toEqual(1);
+it('shows topics box', ()=>{
+  expect(wrapped.find(TopicsBox).length).toEqual(1); //Make sure TopicBox component is inside App component
 });
-//Make sure TopicList component is inside App component
+
+
+it('shows topics list', ()=>{
+  expect(wrapped.find(TopicsList).length).toEqual(1); //Make sure TopicList component is inside App component
+});
---
+++ src/Components/__tests__/TopicsBox.test.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import TopicsBox from 'Components/TopicsBox';
+
+let wrapped;
+
+beforeEach(() => {
+  wrapped = mount(<TopicsBox />);  
+});
+
+afterEach(() => {
+  wrapped.unmount();
+});
+
+it('has a text area and a button', () => {
+  expect(wrapped.find('textarea').length).toEqual(1); //Make sure there is textarea in it
+  expect(wrapped.find('button').length).toEqual(1); //Make sure there is button in it
+});
+
+describe('the text area',() => {
+  beforeEach(() => {
+    wrapped.find('textarea').simulate('change', {
+      target: {value: 'new topics'}
+    }) //Find the textarea and simulate event
+    wrapped.update(); //setstate is asynchronous, we need to wait for the actual render to happen, hence we force our component to render itself
+  });
+  
+  it('has a text area that users can type in', () => {
+    expect(wrapped.find('textarea').prop('value')).toEqual('new topics')
+  });
+
+  it('when form is submitted, text area get emptied', () => {
+    wrapped.find('form').simulate('submit')
+    wrapped.update(); 
+    expect(wrapped.find('textarea').prop('value')).toEqual('')
+  });
+})
